ac3d4f797e033bb232db55fdf1de90368387a09b
Alan Gerber <me@alangerber.us>
2018-11-09 12:10:11 -0500

    Install Peewee and refactor models to models.py

--- customer.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-class Customer:
-    def __init__(self, user_id: int, created: datetime):
-        self.id = user_id
-        self.created = created
-
--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 
+class Customer:
+    def __init__(self, user_id: int, created: datetime):
+        self.id = user_id
+        self.created = created
+
 class Order:
     def __init__(self, order_id: int, order_number: int, user_id: int, created: datetime):
         self.order_id = order_id
         self.order_number = order_number
         self.user_id = user_id
         self.created = created
-
