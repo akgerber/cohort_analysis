fe37078b6cd691ffe42071d4c356770ebaa216fa
Alan Gerber <me@alangerber.us>
2018-11-09 12:20:59 -0500

    Convert models to Peewee

--- models.py
+++ models.py
@@ -1,14 +1,19 @@
-from datetime import datetime
+from peewee import *
 
+database = peewee.SqliteDatabase("customers.db")
 
 class Customer:
-    def __init__(self, user_id: int, created: datetime):
-        self.id = user_id
-        self.created = created
+    """
+    ORM model of customer
+    """
+    id = IntegerField(primary_key=True)
+    created = DateTimeField()
 
 class Order:
-    def __init__(self, order_id: int, order_number: int, user_id: int, created: datetime):
-        self.order_id = order_id
-        self.order_number = order_number
-        self.user_id = user_id
-        self.created = created
+    """
+    ORM model of order
+    """
+    id = IntegerField(primary_key=True)
+    order_number = IntegerField()
+    user_id = ForeignKeyField(Customer, backref='orders')
+    created = DateTimeField()
