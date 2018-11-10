e51972098ff6c2f247db57b43c5d2b2212c2db24
Alan Gerber <me@alangerber.us>
2018-11-10 00:39:54 -0500

    Replace week bucket names with a Cohort class

--- cohort_analysis.py
+++ cohort_analysis.py
@@ -5,9 +5,30 @@ from models import Customer, Order, database
 
 
 class Cohort:
-    def __init__(self, start: datetime, length: timedelta):
+    """A weeklong bucket, extending from `start` to 7 days beyond
+    """
+    def __init__(self, start: arrow):
         self.start = start
-        self.length = length
+
+    def get_end(self):
+        return self.start.shift(weeks=+1)
+
+    def __eq__(self, other):
+        return other.start == self.start
+
+
+def get_cohort_for(dt: datetime, tz: tzinfo = dateutil.tz.gettz('US/Pacific')) -> Cohort:
+    """
+    Place a datetime in a weeklong cohort (with weeks starting/ending in the specified timezone)
+    :param dt: a datetime
+    :param tz: the timezone used to determine week boundaries
+    :return: a Cohort including the datetime
+    """
+    # use Arrow library as builtin datetime doesn't support DST changing over time
+    as_tz = arrow.get(dt).to(tz)
+    iso = as_tz.isocalendar()
+    cohort_start = arrow.Arrow.strptime(f"{iso[0]}|{iso[1]}"+"|1", "%G|%V|%u", tz)
+    return Cohort(cohort_start)
 
 
 def get_start_date() -> datetime:
@@ -26,20 +47,19 @@ def get_end_date() -> datetime:
     return newest.created
 
 
-def get_week_bucket(dt: datetime, tz: tzinfo = dateutil.tz.gettz('US/Pacific')) -> str:
-    """
-    Get a unique bucket name for all datetimes in a week (in the optionally-specific timezone)
-    :param dt: a datetime
-    :param tz: the used to determine week boundaries
-    :return: the bucket name
-    """
-    astz = arrow.get(dt).to(tz)
-    return astz.strftime("%Y%W")
-
-
 if __name__ == "__main__":
     with database:
-        start = get_start_date()
+        start = arrow.get(get_start_date())
+        end = arrow.get(get_end_date())
         print(start)
-        end = get_end_date()
+        startcohort = get_cohort_for(start)
+        print(f"{startcohort.start} {startcohort.get_end()}")
         print(end)
+        endcohort = get_cohort_for(end)
+        print(f"{endcohort.start} {endcohort.get_end()}")
+        cohorts = [startcohort]
+        while startcohort != endcohort:
+            startcohort = get_cohort_for(startcohort.get_end())
+            print(f"{startcohort.start} {startcohort.get_end()}")
+            cohorts.append(startcohort)
+        print(cohorts)
